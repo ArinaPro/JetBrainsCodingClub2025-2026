d3df746f7c18583b5a1d1ec1eec8dad42a28c279
ArinaPro <coolstar.arina@gmail.com>
2025-11-28 17:27:18 +0200

    contest6 C

--- contest6/C_Bear_and_Poker.py
+++ contest6/C_Bear_and_Poker.py
@@ -2,17 +2,19 @@ from math import lcm
 
 n = int(input())
 nums = list(map(int, input().split()))
-lcm_nums = lcm(*nums)
+num0 = -1
 res = "Yes"
 
-for dnum in nums:
-    num = lcm_nums // dnum
+for num in nums:
     while num % 2 == 0:
         num = num // 2
     while num % 3 == 0:
         num = num // 3
-    if num != 1:
-        res = "No"
-        break
+    if num0 == -1:
+        num0 = num
+    else:
+        if num != num0:
+            res = "No"
+            break
 
 print(res)
