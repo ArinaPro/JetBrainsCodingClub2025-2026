7b5b9be91392b7ebbeb15887e2bb735c8a3822e9
ArinaPro <coolstar.arina@gmail.com>
2025-12-08 17:23:26 +0200

    contest8 В just basis, no logic

---
+++ contest8/B_Footbal_Kit.py
@@ -0,0 +1,43 @@
+n = int(input())
+count = [[], []]
+
+for _ in range(n):
+    x, y = map(int, input().split())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from collections import Counter
+#
+# n = int(input())
+# count = [[], []]
+#
+# for _ in range(n):
+#     x, y = map(int, input().split())
+#     count[0].append(x)
+#     count[1].append(y)
+#
+#
+# for ind in range(n):
+#     x, y = 0, 0
+#     for ind2 in range(n-ind):
+#         if x in count[ind2]:
+#             x += 1
+#         if y in count[ind2]:
+#             y += 1
+#     print(f"{x} {y}")
+
